22d3149429ee0459d1234b890fdd5d50f13e7405
James Corbett <wjamescorbett@gmail.com>
2021-11-15 16:51:38 -0500

    Added copyright symbol to footer.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
     return ( 
         <footer className="footer">
-            <h4>Copyright 2021</h4>
+            <h4>Copyright &#169; 2021</h4>
         </footer>
      );
 }
